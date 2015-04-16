f91de7063c9e88aeafc2e873cf903f11f68b3ac4
rLaffuge <dalkspam@gmail.com>
2015-04-16 11:54:59 +0200

    Affichage cam + start button

---
+++ CameraCapture/CameraCapture.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CameraCapture
+{
+    partial class CameraCapture
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CamImageBox = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CamImageBox
+            // 
+            this.CamImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CamImageBox.Location = new System.Drawing.Point(12, 12);
+            this.CamImageBox.Name = "CamImageBox";
+            this.CamImageBox.Size = new System.Drawing.Size(640, 480);
+            this.CamImageBox.TabIndex = 2;
+            this.CamImageBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(540, 520);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(99, 39);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // CameraCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 601);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.CamImageBox);
+            this.Name = "CameraCapture";
+            this.Text = "Camera Output";
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox CamImageBox;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
